a94928d9ce052ea6fe81b930af4ea05e8c21c38d
aaguilar5 <alex_a_aguilar@outlook.com>
2024-03-19 10:36:42 -0700

    imports main activity

--- app/src/main/java/com/example/minifigscroller/MainActivity.kt
+++ app/src/main/java/com/example/minifigscroller/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.minifigscroller.model.Minifig
 import com.example.minifigscroller.ui.theme.MinifigScrollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,3 +49,15 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+ @Composable
+ fun MinifigCard(minifig: Minifig, modifier: Modifier = Modifier) {
+
+     Card(
+         modifier = modifier.padding(8.dp),
+         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+     ){
+
+     }
+
+ }
