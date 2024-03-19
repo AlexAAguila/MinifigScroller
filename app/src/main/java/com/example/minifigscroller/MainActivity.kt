f2a1461257c965202e1049a7ec246209d8243ec9
aaguilar5 <alex_a_aguilar@outlook.com>
2024-03-19 10:43:33 -0700

    Minifig List

--- app/src/main/java/com/example/minifigscroller/MainActivity.kt
+++ app/src/main/java/com/example/minifigscroller/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -56,6 +57,14 @@ fun GreetingPreview() {
     }
 }
 
+ @Composable
+ private fun MinifigList(minifigList: List<Minifig>, modifier: Modifier = Modifier) {
+     LazyColumn {
+         items(minifigList){
+                 minifig -> MinifigCard(minifig)
+         }
+     }
+ }
  @Composable
  fun MinifigCard(minifig: Minifig, modifier: Modifier = Modifier) {
 
